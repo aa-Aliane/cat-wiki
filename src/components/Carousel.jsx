6d47a3e317f193a187c5e937e74c24aaa70ff780
aaliane781@gmail.com <aaliane781@gmail.com>
2022-12-12 22:20:47 +0100

    2c

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,14 +4,27 @@ const Carousel = () => {
   return (
     <div className="carousel">
       <ul className="carousel__list">
-        <li className="carousel__list__item"></li>
-        <li className="carousel__list__item"></li>
-        <li className="carousel__list__item"></li>
-        <li className="carousel__list__item"></li>
+        <li className="carousel__list__item">
+          <div></div>
+          <p className="fs-m fw-semi-bold pt-0">bengal</p>
+        </li>
+        <li className="carousel__list__item">
+          <div></div>
+          <p className="fs-m fw-semi-bold pt-0">savanahh</p>
+        </li>
+        <li className="carousel__list__item">
+          <div></div>
+          <p className="fs-m fw-semi-bold pt-0">norweigian forest cat</p>
+        </li>
+        <li className="carousel__list__item">
+          <div></div>
+          <p className="fs-m fw-semi-bold pt-0">selkirk rex</p>
+        </li>
 
-        <div className="carousel__list__seemore">see more   <span class="material-icons">trending_flat</span></div>
+        <div className="carousel__list__seemore">
+          see more <span class="material-icons">trending_flat</span>
+        </div>
       </ul>
-      
     </div>
   );
 };
--- src/pages/Principale.jsx
+++ src/pages/Principale.jsx
@@ -6,6 +6,7 @@ const Principale = () => {
     <div className="main">
       <div className="main__content">
         <div className="main__content__menu"></div>
+        {/* section 1 */}
         <div className="main__content__first">
           <div>
             <img src="src/assets/CatwikiLogo.svg" alt="" />
@@ -22,12 +23,27 @@ const Principale = () => {
             </div>
           </div>
         </div>
+        {/* section 2 */}
         <div className="main__content__second">
           <div className="message">
-            <span>most</span> searched breed
+            <span>most</span> searched breeds
           </div>
           <div className="message">66+ breeds for you to discover</div>
           <Carousel />
+        </div>
+        {/* section 3*/}
+        <div className="main__content__third">
+          <div>why you should have a cat?</div>
+          <div>
+            Having a cat around you can actually trigger the release of calming
+            chemicals in your body which lower your stress and anxiety leves
+          </div>
+          <div>
+            read more <span class="material-icons">trending_flat</span>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       </div>
     </div>
